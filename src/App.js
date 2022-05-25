7c1a63d8e458e5d1bbd4b4144b7c2455c336789f
ducmanh109` <haducmanh10@gmail.com>
2022-05-26 00:26:09 +0700

    common

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Login from "./screens/Login/Login";
 import Home from "./screens/Home/Home";
